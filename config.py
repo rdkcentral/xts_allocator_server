bb6a933f774b93297a74101f0ab600e5a7f6dd0a
zghp <FKC01@bw-hermes.brwswteam.co.uk>
2024-12-05 14:16:46 +0000

    decrease logo size, add index.html, test scripts for slots allocation and endpoints.md

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    # TODO:
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///xts_allocator.db')
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
