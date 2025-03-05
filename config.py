e92a312da8fba65198f0a92216f5d5ed4a9f0af9
zghp <33546213+zghp@users.noreply.github.com>
2025-03-05 08:58:15 +0000

    add initial work for xts allocator server

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///xts_allocator.db')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+
+    DATABASE_URL = "sqlite:///xts_allocator.db"
